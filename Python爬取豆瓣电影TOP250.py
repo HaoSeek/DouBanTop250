1f6ba80057ef538e3c0dadbc1e326d750d351726
HaoSeek <90230678+HaoSeek@users.noreply.github.com>
2022-04-16 22:47:22 +0800

    第一次提交

--- Python爬取豆瓣电影TOP250.py
+++ Python爬取豆瓣电影TOP250.py
@@ -1,6 +1,5 @@
 import re  # 正则表达式，进行文字匹配
-import urllib.error  # 制定URL，获取网页数据
-import urllib.request
+import urllib.error,urllib.request  # 制定URL，获取网页数据
 
 import xlwt  # 进行excel操作
 from bs4 import BeautifulSoup  # 网页解析，获取数据
